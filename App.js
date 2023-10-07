b9bf9cc5715cdab36efa92ffaa034fc66389b8f3
Rakib Hossen <rakibhossen1660@gmail.com>
2023-10-07 15:59:01 +0600

    create courses page

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
+import Courses from './src/screens/Courses';
 
 export default function App() {
   return (
     <View>
-      <Home />
+      <Courses />
     </View>
   );
 }
